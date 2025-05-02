3a5eee86d45d86fb3ff1c2f8f52a76a75dc1baa4
Jeremy Walworth <walworth@gmail.com>
2025-05-02 15:05:21 -0400

    added allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,  // This enables listening on all addresses
-    port: 80     // Listen on port 80
+    port: 80,     // Listen on port 80
+    allowedHosts: ['hairforecast.app']
   },
 })
